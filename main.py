6f02734c3c2dc50cd2500aceb246089457dd96c5
meghana-choudhary <choudharymeghana1504@gmail.com>
2025-05-24 21:59:42 +0530

    Fix Ports

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def chat(request: ChatRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
 
 
